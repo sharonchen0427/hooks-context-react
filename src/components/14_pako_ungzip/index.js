7e8b8da601920f2f4830e60c49af9f465d05e18d
-chenshumei <-chensharon0427@gmail.com>
2022-01-06 21:50:20 -0500

    download decompressData to txt file

--- src/components/14_pako_ungzip/index.js
+++ src/components/14_pako_ungzip/index.js
@@ -1,18 +1,25 @@
 
 import pako from 'pako'
+import download from 'downloadjs'
 
-export default function UnGzipDemo()   {
+export default function UnGzipDemo() {
 
     var _data = '{"body":{"banners":[],"ptypes":[{"addr":1,"productTypes":[1]},{"addr":2,"productTypes":[2]},{"addr":3,"productTypes":[3]}]},"success":true}';
     var data = pako.gzip(_data);
     console.log("->Pako 压缩data:", data);
-    console.log("->Pako 解压压缩后的data:", pako.ungzip(data, {to: "string"}));//d
-     //https://nodeca.github.io/pako/#ungzip
+    const decompressedData = pako.ungzip(data, { to: "string" })
+    console.log("decompressedData", decompressedData)
+    download(decompressedData,"log_123.log","application/text")
+    //download(data, strFileName, strMimeType);
+    // console.log("->Pako 解压压缩后的data:", pako.ungzip(data, {to: "string"}));//d
+    //https://nodeca.github.io/pako/#ungzip
 
-        return (
-            <div>
-                
-            </div>
-        )
-     
+    return (
+        <div>
+            <p>
+                {decompressedData}
+            </p>
+        </div>
+    )
+
 }
